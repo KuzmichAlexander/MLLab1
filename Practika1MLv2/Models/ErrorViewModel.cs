d7fdb191e39bde9c034a8975d3c37159a57954f9
kuzmich <aleksandr.kz2000@mail.ru>
2024-02-15 23:35:42 +0500

    Готовою без gitignore

---
+++ Practika1MLv2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practika1MLv2.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ Practika1MLv2/Models/RecogResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Practika1MLv2.Models
+{
+    public class RecogResult
+    {
+        public string Label { get; set; }
+        public float Score { get; set; }
+    }
+}
